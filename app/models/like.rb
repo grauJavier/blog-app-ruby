821df02d956eb1e1e8e1335fc07e88c808b5c2e7
Javier Grau <javiergrau.freelance@gmail.com>
2023-09-21 12:59:40 -0300

    Add methods and fix bugs

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,11 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  after_save :update_post_counter
+
+  private
+
+  def update_post_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,16 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name:"user", foreign_key: :author_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :likes, foreign_key: :post_id
   has_many :comments, foreign_key: :post_id
+  after_save :update_post_counter
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_post_counter
+    author.update(posts_counter: author.posts.count)
+  end
 end
