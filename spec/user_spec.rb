cb34e3a629528e22b06fbf1bdd794f5b1a3942be
Javier Grau <javiergrau.freelance@gmail.com>
2023-09-22 10:15:13 -0300

    Add test for 'User.recent_posts'

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -19,4 +19,20 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
   end
+
+  context 'recent_posts' do
+    it 'returns the most recent posts, limited to 3' do
+      user = User.create(name: 'John', posts_counter: 2)
+
+      newest_post = user.posts.create(title: 'Newest Post', text: 'This is the newest post', created_at: Time.now)
+      middle_post = user.posts.create(title: 'Middle Post', text: 'This is a middle post', created_at: 1.day.ago)
+      older_post = user.posts.create(title: 'Old Post', text: 'This is an old post', created_at: 2.days.ago)
+
+      user.posts.create(title: 'Extra Post', text: 'This is an extra post', created_at: 3.days.ago)
+
+      recent_posts = user.recent_posts
+
+      expect(recent_posts).to eq([newest_post, middle_post, older_post])
+    end
+  end
 end
