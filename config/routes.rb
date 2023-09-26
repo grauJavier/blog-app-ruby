acb8fa7ea16a68768456080c27b192698f493dec
Javier Grau <javiergrau.freelance@gmail.com>
2023-09-26 10:47:02 -0300

    Add mock

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'users#index'
 
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
