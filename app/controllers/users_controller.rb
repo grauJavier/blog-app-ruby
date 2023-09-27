a6bc6b4ba2349ca93fe7279b3254e0d1b88a376a
Javier Grau <javiergrau.freelance@gmail.com>
2023-09-27 17:37:07 -0300

    Update controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+
+  def three_most_recent
+    @users = User.order(created_at: :desc).limit(3)
+  end
 end
