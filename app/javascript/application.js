8aa70c9c17e6287ab148c2791d9303ad866318a6
Javier Grau <javiergrau.freelance@gmail.com>
2023-09-27 17:25:45 -0300

    Install 'bootstrap-sass' gem

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require jquery
 //= require bootstrap-sprockets
 
 import "@hotwired/turbo-rails"
