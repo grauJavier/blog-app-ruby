de35a89fccb0f7aa8f639450d2c8334155f2df53
Javier Grau <javiergrau.freelance@gmail.com>
2023-09-27 23:08:19 -0300

    Add 'users' pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(name: 'Alice Johnson', photo: "https://i.pravatar.cc/150?img=#{rand(1..70)}", bio: 'Passionate about photography and travel.')
+User.create(name: 'Ethan Smith', photo: "https://i.pravatar.cc/150?img=#{rand(1..70)}", bio: 'Software developer by day, aspiring musician by night.')
+User.create(name: 'Olivia Davis', photo: "https://i.pravatar.cc/150?img=#{rand(1..70)}", bio: 'Bookworm, tea lover, and art enthusiast.')
+User.create(name: 'Liam Wilson', photo: "https://i.pravatar.cc/150?img=#{rand(1..70)}", bio: 'Fitness enthusiast and certified personal trainer.')
+User.create(name: 'Sophia Lee', photo: "https://i.pravatar.cc/150?img=#{rand(1..70)}", bio: 'Lover of nature, hiking, and sustainable living.')
+
+User.all.each do |user|
+    rand(0..5).times do |i|
+      Post.create(
+        title: "Post #{i + 1} by #{user.name}",
+        text: "This is the content of Post #{i + 1} created by #{user.name}.",
+        author: user
+      )
+    end
+  end
