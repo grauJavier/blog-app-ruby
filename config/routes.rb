62af8ab85838bc7e54a7b7a9c3f349c65e8ca225
Javier Grau <javiergrau.freelance@gmail.com>
2023-09-30 02:37:06 -0300

    Add new post, new comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root 'users#index'
 
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+  resources :users do
+    resources :posts do
+      resources :comments, only: %i[new create]
+      resources :likes, only: %i[new create]
+    end
   end
 end
